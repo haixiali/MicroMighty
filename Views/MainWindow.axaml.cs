981291968c55cb47e36ad0ce32931f0a39ded2c0
hx <leehaixia@outlook.com>
2025-12-11 09:51:32 +0800

    Set up customized title bar

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace MicroMighty.Views;
 
@@ -7,5 +9,33 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        DragArea.PointerPressed += DragArea_PointerPressed;
+        // MyMenu.PointerPressed += DragArea_PointerPressed;
+    }
+    
+    private void DragArea_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            BeginMoveDrag(e);
+        }
+    }
+
+    private void Minimize_Click(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeRestore_Click(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void Close_Click(object? sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
